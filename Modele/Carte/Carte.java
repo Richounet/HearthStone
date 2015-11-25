6dff28adeeaf477e1581622bb0de1a3785bdadbe
Axel <axel.paris69@gmail.com>
2015-11-25 13:52:29 +0100

    -Ajout d'une classe Tools
    -Premiere version modèle de carte/créature/deck

--- Modele/Carte/Carte.java
+++ Modele/Carte/Carte.java
@@ -5,7 +5,34 @@ package Modele.Carte;
  * @author PARIS AXEL    P1306459
  */
 
-public class Carte
+public abstract class Carte
 {
+    private String nom;
+    private int coutRessource;
 
+    public Carte(String nom, int coutRessource)
+    {
+        this.nom = nom;
+        this.coutRessource = coutRessource;
+    }
+
+    public String getNom()
+    {
+        return nom;
+    }
+
+    public void setNom(String nom)
+    {
+        this.nom = nom;
+    }
+
+    public int getCoutRessource()
+    {
+        return coutRessource;
+    }
+
+    public void setCoutRessource(int coutRessource)
+    {
+        this.coutRessource = coutRessource;
+    } 
 }

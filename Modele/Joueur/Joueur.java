a15def10bcfc3bb3607c01375239a318fdd8263a
Axel <axel.paris69@gmail.com>
2015-11-25 14:10:57 +0100

    Nouvelle classe MyObservable

--- Modele/Joueur/Joueur.java
+++ Modele/Joueur/Joueur.java
@@ -1,32 +1,44 @@
 package Modele.Joueur;
 
+import Utilitaire.MyObservable;
+import java.util.Observable;
+
 /**
  * @author RICHE Vincent P1203372
  * @author PARIS AXEL P1306459
  */
-public class Joueur 
+public class Joueur extends MyObservable
 {
     private String nom;
     private int pv;
 
-    public Joueur(String nom, int pv) {
+    public Joueur(String nom, int pv) 
+    {
         this.nom = nom;
         this.pv = pv;
+        
+        Notify();
     }
 
-    public String getNom() {
+    public String getNom() 
+    {
         return nom;
     }
 
-    public int getPv() {
+    public int getPv() 
+    {
         return pv;
     }
 
-    public void setNom(String nom) {
+    public void setNom(String nom) 
+    {
         this.nom = nom;
+        Notify();
     }
 
-    public void setPv(int pv) {
+    public void setPv(int pv) 
+    {
         this.pv = pv;
+        Notify();
     }
 }
---
+++ Utilitaire/MyObservable.java
@@ -0,0 +1,17 @@
+package Utilitaire;
+
+import java.util.Observable;
+
+/**
+ * @author RICHE Vincent P1203372
+ * @author PARIS AXEL    P1306459
+ */
+
+public class MyObservable extends Observable
+{
+    public void Notify()
+    {
+        setChanged();
+	notifyObservers();
+    }
+}

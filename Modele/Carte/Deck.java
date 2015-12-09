2a328539860fbacbe8af5e12ca45ba771edc486f
Riche Vincent <richeounet@gmail.com>
2015-12-09 10:02:19 +0100

    Changement dans le deck

--- Modele/Carte/Deck.java
+++ Modele/Carte/Deck.java
@@ -17,16 +17,17 @@ public class Deck
     public static void InitDeck()
     {
         deck = new ArrayList<>();
-        deck.add(new Creature("Dragon", 4, 4, 6));
+        deck.add(new Creature("Dragon", 4, 9, 6));
         deck.add(new Creature("Gobelin", 1, 1, 1));
-        deck.add(new Creature("Orc", 3, 3, 5));
+        deck.add(new Creature("Orc", 3, 2, 0));
         deck.add(new Creature("Soldat", 2, 2, 2));
-        deck.add(new Creature("Elfe", 3, 3, 5));
-        deck.add(new Creature("Nazgul", 5, 5, 5));
-        deck.add(new Creature("Bilbo", 6, 6, 6));
+        deck.add(new Creature("Elfe", 3, 5, 5));
+        deck.add(new Creature("Nazgul", 10, 8, 5));
+        deck.add(new Creature("Bilbon", 6, 2, 6));
         deck.add(new Creature("Balrog", 10, 10, 6));
-        deck.add(new CreatureRapide("Spider", 1, 1, 2));
-        deck.add(new CreatureRapide("Groot", 1, 0, 4));
+        deck.add(new CreatureRapide("Araignee", 1, 1, 2));
+        deck.add(new CreatureRapide("Loup", 4, 4, 3));
+        deck.add(new CreatureEffetVie("Groot", 1, 0, 4, 6));
         deck.add(new CreatureEffetVie("Sorciere", 1, 3, 4, 2));
         deck.add(new CreatureEffetVie("Mage", 1, 2, 2, 4));
     }
